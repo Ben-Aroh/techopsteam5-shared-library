00e92c4d21772943cb8a546bde83311e58003d29
Ben-Aroh <127508807+Ben-Aroh@users.noreply.github.com>
2023-04-11 23:44:10 +0100

    Create edwardApp.groovy

---
+++ vars/edwardApp.groovy
@@ -0,0 +1,47 @@
+def call(String repoUrl){
+pipeline{
+    agent any
+    stages{
+        stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+        stage('2-parallel-jobs first'){
+            parallel{
+                stage('2-check disk free space in mega byte'){
+                    steps{
+                       sh 'free -m'
+                    }
+                }
+                stage('3-check disk usage'){
+                    steps{
+                        sh 'du -h'
+                    }
+                }
+            }
+        }
+        stage('2-another parallel-jobs second'){
+            parallel{
+                stage('4-check lscpu'){
+                    steps{
+                        sh 'lscpu'
+                    }
+                }
+                stage('5-check disk free space in giga byte'){
+                    steps{
+                      sh 'free -g'
+                    }
+                }
+            }
+        }
+        stage('6-closing stage'){
+            steps{
+                echo "team5 group4 techops are done with multibranch Jenkins build jobs"
+            }
+        }
+    }
+}
+
+}
